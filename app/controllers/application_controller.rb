63d41b16ba73e00a457af217020bed913485c7b8
Maxim Naumkin <naumkin.maks@mail.ru>
2023-02-21 00:07:10 +0300

    Add login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id].present?
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
 end
